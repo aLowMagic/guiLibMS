4f58faa995b1550de82eb403be4ed3dc5795e86f
thePrestige_y <aLowMagic@users.noreply.github.com>
2018-05-30 02:01:25 +0800

    update
    让登陆界面在登陆后关闭，以前无法关闭

--- index.py
+++ index.py
@@ -13,15 +13,20 @@ import pymysql
 from MainWindow import Ui_MainWindow
 import sys
 
-class Ui_title_box(object):
-    def setupUi(self, title_box):
-        title_box.setObjectName("title_box")
-        title_box.setEnabled(True)
-        title_box.resize(600, 400)
-        title_box.setMinimumSize(QtCore.QSize(600, 400))
-        title_box.setMaximumSize(QtCore.QSize(600, 400))
-        title_box.setWindowIcon(QIcon('C:\\Users\\thePr\\Documents\\github\\guiLibMS\\image\\logo.png'))
-        self.title_label = QtWidgets.QLabel(title_box)
+class Ui_title_box(QDialog):
+
+    def __init__(self):
+        super(Ui_title_box, self).__init__()
+        self.setupUi()
+
+    def setupUi(self):
+        self.setObjectName("title_box")
+        self.setEnabled(True)
+        self.resize(600, 400)
+        self.setMinimumSize(QtCore.QSize(600, 400))
+        self.setMaximumSize(QtCore.QSize(600, 400))
+        self.setWindowIcon(QIcon('C:\\Users\\thePr\\Documents\\github\\guiLibMS\\image\\logo.png'))
+        self.title_label = QtWidgets.QLabel(self)
         self.title_label.setGeometry(QtCore.QRect(175, 80, 251, 41))
         font = QtGui.QFont()
         font.setFamily("微软雅黑")
@@ -30,22 +35,22 @@ class Ui_title_box(object):
         font.setWeight(75)
         self.title_label.setFont(font)
         self.title_label.setObjectName("title_label")
-        self.input_button = QtWidgets.QPushButton(title_box)
+        self.input_button = QtWidgets.QPushButton(self)
         self.input_button.setGeometry(QtCore.QRect(240, 260, 121, 31))
         font = QtGui.QFont()
         font.setFamily("微软雅黑")
         self.input_button.setFont(font)
         self.input_button.setObjectName("input_button")
-        self.label = QtWidgets.QLabel(title_box)
+        self.label = QtWidgets.QLabel(self)
         self.label.setGeometry(QtCore.QRect(170, 150, 54, 12))
         self.label.setObjectName("label")
-        self.label_2 = QtWidgets.QLabel(title_box)
+        self.label_2 = QtWidgets.QLabel(self)
         self.label_2.setGeometry(QtCore.QRect(175, 200, 54, 12))
         self.label_2.setObjectName("label_2")
-        self.lineEdit = QtWidgets.QLineEdit(title_box)
+        self.lineEdit = QtWidgets.QLineEdit(self)
         self.lineEdit.setGeometry(QtCore.QRect(240, 140, 200, 30))
         self.lineEdit.setObjectName("lineEdit")
-        self.lineEdit_2 = QtWidgets.QLineEdit(title_box)
+        self.lineEdit_2 = QtWidgets.QLineEdit(self)
         self.lineEdit_2.setGeometry(QtCore.QRect(240, 190, 200, 30))
         self.lineEdit_2.setObjectName("lineEdit_2")
         self.lineEdit_2.setEchoMode(QLineEdit.Password)
@@ -53,25 +58,20 @@ class Ui_title_box(object):
         self.otherMsgBox = QMessageBox(QMessageBox.Information, "未知错误", "未知错误", QMessageBox.Ok)
 
 
-        self.retranslateUi(title_box)
+        self.retranslateUi()
         self.input_button.clicked.connect(self.indexJudge)
-        QtCore.QMetaObject.connectSlotsByName(title_box)
+        QtCore.QMetaObject.connectSlotsByName(self)
 
 
-    def retranslateUi(self, title_box):
+    def retranslateUi(self):
         _translate = QtCore.QCoreApplication.translate
-        title_box.setWindowTitle(_translate("title_box", "XXXX图书馆管理系统"))
+        self.setWindowTitle(_translate("title_box", "XXXX图书馆管理系统"))
         self.title_label.setText(_translate("title_box", "XXXX图书馆管理系统"))
         self.input_button.setText(_translate("title_box", "登陆"))
         self.label.setText(_translate("title_box", "用户名"))
         self.label_2.setText(_translate("title_box", "密码"))
 
 
-    def loginSucceed(self, userName, userKey):
-        self.mainPara = QtWidgets.QWidget()
-        self.main_Window = Ui_MainWindow()
-        self.main_Window.setupMainWindow(self.mainPara, userName, userKey)
-        self.mainPara.show()
 
     def indexJudge(self):
         try:
@@ -82,10 +82,17 @@ class Ui_title_box(object):
             cur = conn.cursor()
             sql = "select * from manage_list where manage_name = '%s' and manage_key = '%s';" % (userName, userKey)
             if cur.execute(sql) != 0:
-                self.loginSucceed(userName, userKey)
-                self.lineEdit.clear()
-                self.lineEdit_2.clear()
+                self.accept()
             else:
                 self.msgBox.show()
         except:
             self.otherMsgBox.show()
+
+    def ret(self):
+        name = self.lineEdit.text()
+        key = self.lineEdit_2.text()
+        information = {'name': name, 'key': key}
+        return information
+
+    def closeEvent(self, a0: QtGui.QCloseEvent):
+        self.close()
--- start.py
+++ start.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from index import Ui_title_box
+from MainWindow import Ui_MainWindow
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QMessageBox, QApplication, QWidget, QDialog
@@ -10,8 +11,13 @@ from PyQt5 import QtWidgets
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    dialog = QtWidgets.QDialog()
     loginWindow = Ui_title_box()
-    loginWindow.setupUi(dialog)
-    dialog.show()
-    sys.exit(app.exec_())
+    if loginWindow.exec_() == 1:
+        information = loginWindow.ret()
+        name = information['name']
+        key = information['key']
+        dialog = QtWidgets.QWidget()
+        mainWindow = Ui_MainWindow()
+        mainWindow.setupMainWindow(dialog, name, key)
+        dialog.show()
+        sys.exit(app.exec_())
